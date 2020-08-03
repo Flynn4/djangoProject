6fe22ac399a241b45b431c23d3e0d50822709072
Flynn4 <hmf245388320@qq.com>
2020-08-03 23:02:59 +0800

    Initial commit

---
+++ wbl/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    # Add all details into dict
+    dict = {}
+    return render(request, 'wbl/index.html', dict)
+
+
+def dashboard(request):
+    return render(request, 'wbl/dashboard.html')
