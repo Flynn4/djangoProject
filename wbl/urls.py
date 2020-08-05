2938781bbeed87bfa6d6cb30ce7d9e4eeb3859f5
Flynn4 <hmf245388320@qq.com>
2020-08-06 03:29:38 +0800

    Update

--- wbl/urls.py
+++ wbl/urls.py
@@ -5,4 +5,11 @@ app_name = 'wbl'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/', views.profile, name='profile'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('task-add/', views.task_add, name='task_add'),
+    path('task-edit/', views.task_edit, name='task_edit'),
+    path('task-detail/', views.task_detail, name='task_detail'),
+    path('peer-review/', views.peer_review, name='peer_review'),
 ]
--- wbl/views.py
+++ wbl/views.py
@@ -8,6 +8,29 @@ def index(request):
     dict = {}
     return render(request, 'wbl/index.html', dict)
 
+def profile(request):
+    return render(request, 'wbl/profile.html')
+
 
 def dashboard(request):
     return render(request, 'wbl/dashboard.html')
+
+
+def tasks(request):
+    return render(request, 'wbl/tasks.html')
+
+
+def task_add(request):
+    return render(request, 'wbl/task-add.html')
+
+
+def task_edit(request):
+    return render(request, 'wbl/task-edit.html')
+
+
+def task_detail(request):
+    return render(request, 'wbl/task-detail.html')
+
+
+def peer_review(request):
+    return render(request, 'wbl/peer-review.html')
